b9e54b1c57aac262da0747e0feea54bad10445ea
ai_plus2 <1030422807@qq.com>
2023-09-14 23:10:18 +0800

    my commmit

---
+++ Jungle Server/游戏服务器端/Controller/GameController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+using GameServer.Servers;
+
+namespace GameServer.Controller
+{
+    internal class GameController : BaseController
+    {
+        public GameController()
+        {
+            requestCode = RequestCode.Game;
+        }
+
+        public string StartGame(string data, Client client, Server server)
+        {
+            if (client.IsHouseOwner())
+            {
+                Room room = client.Room;
+                Console.WriteLine("房间" + room.ToString() + "开始游戏");
+                room.BroadcastMessage(client, ActionCode.StartGame, ((int)ReturnCode.Success).ToString());
+                room.StartTimer();
+                return ((int)ReturnCode.Success).ToString();
+            }
+            else
+            {
+                return ((int)ReturnCode.Fail).ToString();
+            }
+        }
+
+        public string Move(string data, Client client, Server server)
+        {
+            Console.WriteLine("客户端" + client.ToString() + "移动");
+            Room room = client.Room;
+            if (room != null)
+                room.BroadcastMessage(client, ActionCode.Move, data);
+            return null;
+        }
+
+        public string Shoot(string data, Client client, Server server)
+        {
+            Console.WriteLine("客户端" + client.ToString() + "射击");
+            Room room = client.Room;
+            if (room != null)
+                room.BroadcastMessage(client, ActionCode.Shoot, data);
+            return null;
+        }
+
+        public string Attack(string data, Client client, Server server)
+        {
+            Console.WriteLine("客户端" + client.ToString() + "受到伤害");
+            int damage = int.Parse(data);
+            Room room = client.Room;
+            if (room == null) return null;
+            room.TakeDamage(damage, client);
+            return null;
+        }
+
+        public string QuitBattle(string data, Client client, Server server)
+        {
+            Room room = client.Room;
+
+            if (room != null)
+            {
+                room.BroadcastMessage(null, ActionCode.QuitBattle, "r");
+                room.Close();
+            }
+            return null;
+        }
+    }
+}
---
+++ Jungle Server/游戏服务器端/Servers/Server.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using GameServer.Controller;
+using Common;
+using System.Runtime.InteropServices;
+
+namespace GameServer.Servers
+{
+    internal class Server
+    {
+        private IPEndPoint ipEndPoint;
+        private Socket serverSocket;
+        private List<Client> clientList = new List<Client>();
+        private List<Room> roomList = new List<Room>();
+        private ControllerManager controllerManager;
+
+        public Server()
+        { }
+
+        public Server(string ipStr, int port)
+        {
+            controllerManager = new ControllerManager(this);
+            SetIpAndPort(ipStr, port);
+        }
+
+        public void SetIpAndPort(string ipStr, int port)
+        {
+            ipEndPoint = new IPEndPoint(IPAddress.Parse(ipStr), port);
+        }
+
+        public void Start()
+        {
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket.Bind(ipEndPoint);
+            serverSocket.Listen(0);
+            serverSocket.BeginAccept(AcceptCallBack, null);
+        }
+
+        private void AcceptCallBack(IAsyncResult ar)
+        {
+            Socket clientSocket = serverSocket.EndAccept(ar);
+            Client client = new Client(clientSocket, this);
+            client.Start();
+            clientList.Add(client);
+            Console.WriteLine("客户端接入" + client.ToString());
+            serverSocket.BeginAccept(AcceptCallBack, null);
+        }
+
+        public void RemoveClient(Client client)
+        {
+            lock (clientList)
+            {
+                clientList.Remove(client);
+            }
+        }
+
+        public void SendResponse(Client client, ActionCode actionCode, string data)
+        {
+            client.Send(actionCode, data);
+        }
+
+        public void HandleRequest(RequestCode requestCode, ActionCode actionCode, string data, Client client)
+        {
+            controllerManager.HandleRequest(requestCode, actionCode, data, client);
+        }
+
+        public void CreateRoom(Client client)
+        {
+            Console.WriteLine("客户端" + client.ToString() + "创建了房间");
+            Room room = new Room(this);
+            room.AddClient(client);
+            roomList.Add(room);
+        }
+
+        public void RemoveRoom(Room room)
+        {
+            if (roomList != null && room != null)
+            {
+                roomList.Remove(room);
+            }
+        }
+
+        public List<Room> GetRoomList()
+        {
+            return roomList;
+        }
+
+        public Room GetRoomById(int id)
+        {
+            foreach (Room room in roomList)
+            {
+                if (room.GetId() == id) return room;
+            }
+            return null;
+        }
+    }
+}
